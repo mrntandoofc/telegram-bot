cb58d96901350ce8eb6e0eebf30559ffcd67356d
Gifted Maurice <166598671+mouricedevs@users.noreply.github.com>
2025-02-04 14:32:59 +0300

    Gifted

---
+++ set.js
@@ -0,0 +1,30 @@
+global.prefix = '/'; // Set Your Preferred Prefix
+global.botName = 'GIFTED-MD'; // Bot Name
+global.botVersion = '2.0.0';
+global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
+global.botToken = ''; // Bot Token (Get it from BotFather)
+global.ownerUsername = 'mouricedevs'; // Owner Username(For Bot Owner)
+global.timeZone = 'Africa/Nairobi'; // Your Time Zone
+global.ownerId = [5153324742]; // Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by GiftedTech*';
+global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
+global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
+global.giftedKey = ''; // Gifted Api Key
+global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
+global.maherKey = ''; // NexOracle Api Key
+global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+global.port = '7000';
+
+global.giftechMess = {
+    internalerr: 'Internal Server Error!',
+    owner: 'Owner-Only Feature!',
+    group: 'Use in a Group!',
+    private: 'Use in a Private Chat!',
+    wait: 'Please Wait...',
+    done: 'Completed Successfully!',
+    error: 'Oops, an Error Occurred'
+};
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
